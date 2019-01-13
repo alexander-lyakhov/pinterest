3544c2f9b3fc13745af319005ba380f46785e82c
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2019-01-13 17:42:22 +0200

    first commit

---
+++ .settings/vue.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+var projectName = process.env.PROJECT_NAME
+var projectPath = path.resolve(__dirname, 'apps', projectName);
+
+module.exports = {
+
+	configureWebpack: config => {
+		config.entry.app = [path.resolve(projectPath, 'src/main.js')];
+		config.context = __dirname;
+		config.resolve.alias['@'] = path.resolve(projectPath, 'src');
+	},
+
+	chainWebpack: config => {
+		config
+			.plugin('html')
+			.tap(args => {
+				args[0].template = path.resolve(projectPath, 'public/index.html')
+				return args
+			})
+
+		config
+			.plugin('copy')
+			.tap(args => {
+				return [[{
+					from: path.resolve(projectPath, 'public'),
+					to: path.resolve(projectPath, 'dist'),
+					toType: 'dir',
+					ignore: ['index.html', '.DS_Store']
+				}]]
+			})
+	},
+
+	outputDir: path.resolve(projectPath, 'dist'),
+
+	baseUrl: process.env.NODE_ENV === 'production'
+		? `/${projectName}/`
+		: '/',
+
+	lintOnSave: false
+}
---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+	</div>
+</template>
+
+<script>
+	//import NavBar from '@/components/NavBar.vue';
+
+	export default {
+		name: 'App',
+
+		/*
+		components: {
+			NavBar
+		},
+		*/
+
+		mounted() {
+			console.log('App @', @)
+		}
+	}
+<script>
+
+<style lang="scss" scoped>
+</style>
